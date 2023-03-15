c8e357cee6b056067e0bf45ec508eeff05ea9a06
Shahriyar Aghajani <shahriyar.a13@gmail.com>
2023-03-15 12:02:07 +0100

    config

---
+++ src/main/kotlin/com/github_shahriyar13/ecommerce/presentation/common/ControllersPaths.kt
@@ -0,0 +1,5 @@
+package com.github_shahriyar13.ecommerce.presentation.common
+
+object ControllersPaths {
+    const val baseApiPath = "/api/"
+}
---
+++ src/main/kotlin/com/github_shahriyar13/ecommerce/presentation/common/WebSecurityConfig.kt
@@ -0,0 +1,38 @@
+package com.github_shahriyar13.ecommerce.presentation.common
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig {
+
+    @Bean
+    @Throws(Exception::class)
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeHttpRequests { requests ->
+            requests.requestMatchers("/", "/sign-in", "/sign-up", "/api/account/v1/sign-in", "/api/account/v1/sign-up")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+        }
+        return http.build()
+    }
+
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val user: UserDetails = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build()
+        return InMemoryUserDetailsManager(user)
+    }
+}
